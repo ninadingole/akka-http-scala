e226c30dbaf22c00aa208aedeb3709e2d1a8f680
Ninad Ingole <needna78@gmail.com>
2018-04-04 23:46:59 +0530

    Final Work on Async Handler

--- build.sbt
+++ build.sbt
@@ -10,6 +10,7 @@ val akkaVersion = "2.5.11"
 libraryDependencies += "com.typesafe.akka" %% "akka-http"   % akkaHttpVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-actor"  % akkaVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.11"
+libraryDependencies += "com.typesafe.akka" %% "akka-http2-support" % akkaHttpVersion
 libraryDependencies += "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test
 libraryDependencies += "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion     % Test
 
--- src/main/scala/com/sample/async/AsyncRouteHandler.scala
+++ src/main/scala/com/sample/async/AsyncRouteHandler.scala
@@ -3,7 +3,9 @@ package com.sample.async
 import akka.actor.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpMethods._
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.settings.ServerSettings
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
@@ -11,32 +13,33 @@ import scala.concurrent.Future
 import scala.io.StdIn
 
 object AsyncRouteHandler extends App {
-  val config                      = ConfigFactory.load()
-  implicit val system             = ActorSystem("blocking-ex", config)
-  implicit val materialzier       = ActorMaterializer()
-  implicit val blockingDispatcher = system.dispatchers.lookup("blocking-io-dispatcher")
-  val logger                      = Logging(system, getClass)
-//  implicit val executionContext = system.dispatcher  // Dont use this as its a http dispatcher and will block akka system
+  val config                    = ConfigFactory.load()
+  implicit val system           = ActorSystem("blocking-ex", config)
+  implicit val materialzier     = ActorMaterializer()
+  val blockingDispatcher        = system.dispatchers.lookup("blocking-io-dispatcher")
+  val logger                    = Logging(system, getClass)
+  val serverSettings            = ServerSettings(config)
+  implicit val executionContext = system.dispatcher
 
-  def doBlockingIO(): Future[String] = {
+  def doBlockingIO(): Future[HttpResponse] = {
     Future {
-      logger.info("Will block for 4 sec...")
+//    logger.info("Will block for 4 sec...")
       Thread.sleep(4000)
       logger.info("sending response")
-      "Done Blocking, Here is the response."
+      HttpResponse(status = StatusCodes.OK, entity = "Done Blocking, Here is the response.")
+    }(blockingDispatcher)
+  }
+
+  val asyncHandler: HttpRequest => Future[HttpResponse] = {
+    case HttpRequest(GET, Uri.Path("/async"), _, _, _) => {
+      logger.info("calling io")
+      val future = doBlockingIO()
+      logger.info("done calling io")
+      future
     }
   }
 
-  val routes = (httpRequest: HttpRequest) =>
-    Future[HttpResponse] {
-
-      val future: Future[String] = doBlockingIO()
-      logger.info("IO call done...")
-      HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, "Hello"))
-
-  }
-
-  val future = Http().bindAndHandleAsync(routes, "localhost", 8080)
+  val future = Http().bindAndHandleAsync(asyncHandler, "localhost", 8080, settings = serverSettings)
   logger.info("Server Started @8080\nPress Enter to Stop...")
   StdIn.readLine()
   future
