c60eaca6f20493274543d6bd9789e3db3aeeeaf2
Ninad Ingole <needna78@gmail.com>
2018-04-05 23:00:34 +0530

    Add Example for POST request Swagger Documentation

--- src/main/scala/com/sample/swagger/ISimpleAPI.scala
+++ src/main/scala/com/sample/swagger/ISimpleAPI.scala
@@ -1,15 +1,14 @@
 package com.sample.swagger
 
-import akka.http.scaladsl.server
 import io.swagger.annotations._
-import javax.ws.rs.Path
+import javax.ws.rs.{Path, PathParam}
 import spray.json._
 
 @Api(value = "/simple", produces = "application/json")
 @Path("/hello")
 trait ISimpleAPI {
 
-  @ApiOperation(value = "Returing Hello World", httpMethod = "GET", response = classOf[Response])
+  @ApiOperation(value = "Returing Hello World", httpMethod = "GET", response = classOf[HelloMessage])
   @Path("/{name}")
   @ApiImplicitParams(
     Array(
@@ -17,12 +16,17 @@ trait ISimpleAPI {
                            value = "Name of the person to Greet",
                            defaultValue = "ninad",
                            required = true,
-                           dataType = "string",
+                           dataTypeClass = classOf[String],
                            paramType = "path")
     )
   )
-  @ApiResponses(Array(new ApiResponse(code = 200, message = "Return hello response", response = classOf[Response])))
-  def getHello: server.Route
+  @ApiResponses(Array(new ApiResponse(code = 200, message = "Return hello response", response = classOf[HelloMessage])))
+  def getHello(@PathParam(value = "name") name: String): HelloMessage
+
+  @ApiOperation(value = "Returing Hello World", httpMethod = "POST", response = classOf[HelloMessage])
+  @Path("/")
+  @ApiResponses(Array(new ApiResponse(code = 200, message = "Return hello response", response = classOf[HelloMessage])))
+  def postHello(postRequest: PostRequest): HelloMessage
 }
 
 trait Response
@@ -31,4 +35,10 @@ case class HelloMessage(message: String) extends Response
 
 object HelloMessageSupport extends DefaultJsonProtocol {
   implicit val format = jsonFormat1(HelloMessage)
+}
+
+case class PostRequest(name: String)
+
+object PostRequestSupport extends DefaultJsonProtocol {
+  implicit val postRequest = jsonFormat1(PostRequest)
 }
