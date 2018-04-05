79d8643e9b3d7f699b6ccfb2e6df0847e45de82f
Ninad Ingole <needna78@gmail.com>
2018-04-05 20:52:46 +0530

    Add Redoc UI Support For Rest API documentation

--- src/main/scala/com/sample/swagger/Site.scala
+++ src/main/scala/com/sample/swagger/Site.scala
@@ -5,5 +5,6 @@ import akka.http.scaladsl.server.Directives
 trait Site extends Directives {
   val site =
   path("swagger") { getFromResource("swagger/index.html") } ~
-  getFromResourceDirectory("swagger") ~ getFromDirectory("docs/src/main")
+  getFromResourceDirectory("swagger") ~ getFromDirectory("docs/src/main") ~
+  path("redoc") { getFromResource("redoc/index.html") }
 }
